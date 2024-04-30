29da7e7201127e2130e15b5676122cbf587eab18
ashishnambiar <ashishleo1998@gmail.com>
2024-04-30 11:22:42 +0530

    day2 part2

--- day2/bin/day2.dart
+++ day2/bin/day2.dart
@@ -1,14 +1,20 @@
 import 'dart:io';
 
 void main(List<String> arguments) {
-  final f = File('bin/input.txt');
+  if (arguments.isEmpty) {
+    print("missing args");
+    return;
+  }
+  final f = File(arguments[0]);
   final values = <int>{};
+  final List<int> maxCount = [];
 
   for (var (_, e) in f.readAsLinesSync().indexed) {
     final game = e.split(':');
     final gameNum = game.first.split(' ')[1];
     final rounds = game[1].split(';');
     final List<bool> l = [];
+    final Map<ColorType, int> maxPair = {};
 
     for (var e in rounds) {
       final colors = e.split(',');
@@ -18,16 +24,22 @@ void main(List<String> arguments) {
         final p = c.trim().split(' ');
         final color = p[1].colorType ?? ColorType.red;
         colorPair.addAll({color: int.parse(p[0])});
+
+        if ((maxPair[color] ?? 0) < int.parse(p[0])) {
+          maxPair[color] = int.parse(p[0]);
+        }
       }
 
       l.add(!colorPair.entries.any((e) => e.value > e.key.max));
     }
+    maxCount.add(maxPair.entries.fold<int>(1, (p, e) => p * e.value));
 
     if (l.fold<bool>(true, (p, e) => p && e)) {
       values.add(int.parse(gameNum));
     }
   }
-  stdout.write(values.fold<int>(0, (p, e) => p + e));
+  stdout.write(maxCount.fold<int>(0, (p, e) => p + e));
+  // stdout.write(values.fold<int>(0, (p, e) => p + e));
 }
 
 enum ColorType {
