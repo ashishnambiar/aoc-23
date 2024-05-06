5a5c0d46b7949ff57d8142dfdf8f545bba443db9
ashishnambiar <ashishleo1998@gmail.com>
2024-05-06 19:40:17 +0530

    day4 part2

--- day4/bin/day4.dart
+++ day4/bin/day4.dart
@@ -9,16 +9,33 @@ void main(List<String> arguments) {
   final f = File(arguments[0]);
 
   final values = <int>[];
-  for (final line in f.readAsLinesSync()) {
+  int r = 0;
+  var v = <int>[f.readAsLinesSync().length];
+  for (final (i, line) in f.readAsLinesSync().indexed) {
     final input = line.split(':')[1].split('|').map((e) => e.trim()).toList();
     final right = input[1].split(RegExp(r'\s+')).toSet();
     final left = input[0].split(RegExp(r'\s+')).toSet();
 
-    values.add(pow(2, (left.intersection(right).length - 1)).toInt());
+    final common = left.intersection(right).length;
+
+    // add the amount
+    r += v.length;
+
+    // add the numbers
+    v.addAll(List.generate(v.length, (index) => common + i));
+
+    // remove the invalid cards
+    v = v.where((e) {
+      return e != i;
+    }).toList();
+
+    values.add(pow(2, (common - 1)).toInt());
   }
 
   final res =
       values.fold<int>(0, (previousValue, element) => previousValue + element);
 
   stdout.write(res);
+  stdout.write(' ');
+  stdout.write(r);
 }
